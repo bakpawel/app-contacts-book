2e02a28c85dc2b512c6da03e8708436450f607e2
bakpawel <bakpawel.poczta@gmail.com>
2021-08-17 20:39:17 +0200

    created contacts and contacts-list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactsListComponent } from './contacts/contacts-list/contacts-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    ContactsListComponent
   ],
   imports: [
     BrowserModule,
