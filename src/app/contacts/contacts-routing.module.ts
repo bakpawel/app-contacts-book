bb21488dfca1051ebe89929cee8eef11158fb82f
bakpawel <bakpawel.poczta@gmail.com>
2022-02-07 20:15:16 +0100

    error massages added

---
+++ src/app/contacts/contacts-routing.module.ts
@@ -0,0 +1,26 @@
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Route } from '@angular/router';
+// import { ContactDetailsComponent } from './contact-details/contact-details.component';
+// import { ContactAddComponent } from './contact-add/contact-add.component';
+// import { ContactsListComponent } from './contacts-list/contacts-list.component';
+// import { ContactDetailsStartComponent } from './contact-details-start/contact-details-start.component';
+
+// const CONTACTS_ROUTES: Route[]=[
+//     {path: '',redirectTo:'/contacts', pathMatch: 'full'},
+//     { path: 'contacts', component: ContactsListComponent, children: [
+//         { path: '', component: ContactDetailsStartComponent },
+//         { path: 'contact-details/:id', component: ContactDetailsComponent }
+//     ]},
+//     { path: 'contact-add', component: ContactAddComponent },
+//   ];
+
+// @NgModule({
+//     imports: [
+//         RouterModule.forChild(CONTACTS_ROUTES)
+//     ],
+//     exports: [
+//         RouterModule
+//     ]
+// })
+
+// export class ContactsRoutingModule{}
